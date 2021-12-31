24a0e7f10095c06b058a36a7b272bce3096eaa28
VladimirKivg <cheremuha1@gmal.com>
2021-12-31 20:10:05 +0200

    fix

--- src/main/java/com/example/bookShop/pojo/UserPojo.java
+++ src/main/java/com/example/bookShop/pojo/UserPojo.java
@@ -1,11 +1,15 @@
 package com.example.bookShop.pojo;
 
-
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.Iterator;
 
-
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "user")
 public class UserPojo  {
     @Id
@@ -14,36 +18,5 @@ public class UserPojo  {
     String name;
     String firstName;
 
-    public UserPojo(int id, String name, String firstName) {
-        this.id = id;
-        this.name = name;
-        this.firstName = firstName;
-    }
 
-    public UserPojo() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
 }
